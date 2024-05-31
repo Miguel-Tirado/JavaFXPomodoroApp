ac8942029855e29624def219480fe6c16cd2a3f5
MiguelTirado <miguel.e.tirado11@gmail.com>
2024-05-30 18:36:24 -0700

    updateded Stage title

--- src/main/java/com/example/pomofx_win/PomoApplication.java
+++ src/main/java/com/example/pomofx_win/PomoApplication.java
@@ -12,7 +12,7 @@ public class PomoApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PomoApplication.class.getResource("pomo-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Pomodoro FX");
         stage.setScene(scene);
         stage.show();
     }
